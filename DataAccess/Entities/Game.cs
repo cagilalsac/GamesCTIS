7f65a2bff424da156ff325f991caa5162a3c18c0
Çağıl Alsaç <cagilalsac@outlook.com>
2024-02-01 14:28:03 +0300

    Game and User entities created in DataAccess. Properties explained.

---
+++ DataAccess/Entities/Game.cs
@@ -0,0 +1,23 @@
+﻿namespace DataAccess.Entities
+{
+    public class Game
+    {
+        //private int _id; // field
+
+        //public void SetId(int id) // behaviors, setter
+        //{
+        //    _id = id;
+        //}
+
+        //public int GetId() // behaviors, getter
+        //{
+        //    return _id;
+        //}
+
+        public int Id { get; set; } // property
+        public string Guid { get; set; }
+        public string Name { get; set; }
+        public DateTime PublishDate { get; set; }
+        public decimal TotalSalesPrice { get; set; } // double, float
+    }
+}
---
+++ DataAccess/Entities/User.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAccess.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Guid { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        //public Statuses Status { get; set; } // junior, senior, master
+    }
+}
