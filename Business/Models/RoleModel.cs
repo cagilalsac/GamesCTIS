b5a43f710962d0350c1f154a5dcdf069df74f7f9
Çağıl Alsaç <cagilalsac@outlook.com>
2024-02-22 12:36:58 +0300

    MVC default route structure, HTML and Tag Helpers in the Roles -> Index view of the MVC Project.

--- Business/Models/RoleModel.cs
+++ Business/Models/RoleModel.cs
@@ -1,12 +1,14 @@
 ﻿#nullable disable
 
 using DataAccess.Records.Bases;
+using System.ComponentModel;
 
 namespace Business.Models
 {
     public class RoleModel : Record
     {
         #region Entity Properties
+        [DisplayName("Role Name")]
         public string Name { get; set; }
         #endregion
     }
